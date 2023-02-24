b301959fda1b9fbc0bfd6284ac23b6ffdbe7e90e
Fayozbek <120027251+fayzullayevv@users.noreply.github.com>
2023-02-24 16:34:16 +0500

    Add files via upload

---
+++ begin.py
@@ -0,0 +1,289 @@
+# 1
+# def Perimetr(a):
+#   return 4*a
+
+# print(Perimetr(a=int(input())))
+
+
+# 2
+# def Yuza():
+#     a = int(input())
+#     return a**2
+
+# print(Yuza())
+
+
+# 3
+# a = int(input())
+# b = int(input())
+# print(f"S = {a*b}")
+# print(f"2*{a+b}")
+
+
+# 4
+# def Diametr(d):
+#   pi = 3.14
+#   return pi*d
+
+# print(Diametr(d=int(input())))
+
+
+# 5
+# a = int(input())
+# print(f"Hajmi = {a**3}")
+# print(f"Sirti = {6*a**2}")
+
+
+# 6
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(f"Hajmi = {a*b*c}")
+# print(f"Sirti = {2*(a*b+b*c+a*c)}")
+
+
+# 7
+# r = int(input())
+# pi = 3.14
+# print(f"l = {2*pi*r}")
+# print(f"S = {pi*r**2}")
+
+
+# 8
+# def arifmetik(a,b):
+#   return (a+b)/2
+
+# print(arifmetik(a=int(input()),b=int(input())))
+
+
+# 9
+# import math
+# def geometrik(a,b):
+#   return math.sqrt(a*b)
+
+# print(geometrik(a=int(input()),b=int(input())))
+
+
+# 10
+# a = int(input())
+# b = int(input())
+# print(f"Yig'indisi: {a+b}")
+# print(f"Kopaytmasi: {a*b}")
+# print(f"Kvadrati a: {a**2} b: {b**2}")
+
+
+# 11
+# a = int(input())
+# b = int(input())
+# print(f"Yig'indisi: {a+b}")
+# print(f"Kopaytmasi: {a*b}")
+# print(f"Moduli a: {abs(a)} b: {abs(b)}")
+
+
+# 12
+# import math
+# a = int(input())
+# b = int(input())
+# c = math.sqrt(a**2+b**2)
+# print(f"C = {c}")
+# print(f"P = {a+b+c}")
+
+
+# 13
+# r1 = int(input())
+# r2 = int(input())
+# pi = 3.14
+# print(f"S1 = {pi*r1}")
+# print(f"S2 = {pi*r2}")
+# print(f"S3 = {pi*(r1-r2)}")
+
+
+# 14
+# r = int(input())
+# pi = 3.14
+# print(f"L = {2*pi*r}")
+# print(f"S = {pi*r**2}")
+
+
+# 16
+# def masofa(x1,x2):
+#   return abs(x2-x1)
+
+# print(masofa(x1=int(input()),x2=int(input())))
+
+
+# 20
+# import math
+# def sum():
+#   x1 = int(input())
+#   x2 = int(input())
+#   y1 = int(input())
+#   y2 = int(input())
+#   return math.sqrt((x2-x1)**2+(y2-y1)**2)
+
+# print(sum())
+
+
+# 21
+# import math 
+# x1 = int(input())
+# y1 = int(input())
+# x2 = int(input())
+# y2 = int(input())
+# x3 = int(input())
+# y3 = int(input())
+# a = math.sqrt((x2-x1)**2+(y2-y1)**2)
+# b = math.sqrt((x3-x2)**2+(y3-y2)**2)
+# c = math.sqrt((x1-x2)**2+(y1-y2)**2)
+# p = (a+b+c)/2
+# print(math.sqrt(p*(p-a)*(p-b)*(p-c)))
+
+
+# 22
+# a = int(input())
+# b = int(input())
+# c = a=b
+# print(c)
+
+
+# 23
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = a=b
+# e = b=c
+# f = c=a
+# print(d,e,f)
+
+
+# 24
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = a=c
+# e = b=a
+# f = c=b
+# print(d,e,f)
+
+
+# 25
+# def function():
+#     x = int(input())
+#     return 3*x**6-6*x**2-7
+
+# print(function())
+
+
+# 26
+# def function():
+#     x = int(input())
+#     return 4*(x-3)**6-7*(x-3)**3+2
+
+# print(function())
+
+
+# 27
+# a = int(input())
+# print(f"a2 = {a**2}")
+# print(f"a4 = {a**4}")
+# print(f"a8 = {a**8}")
+
+
+# 28
+# a = int(input())
+# print(f"a2 = {a**2}")
+# print(f"a3 = {a**3}")
+# print(f"a5 = {a**5}")
+# print(f"a10 = {a**10}")
+# print(f"a815 = {a**15}")
+
+
+# 29
+# def new_func():
+#     a = int(input())
+#     pi = 3.14
+#     radian = a*(pi/180)
+#     return radian
+
+# print(new_func())
+
+
+# 30
+# def new_func():
+#     a = int(input())
+#     pi = 3.14
+#     gradus = a*(180/pi)
+#     return gradus
+
+# print(new_func())
+
+
+# 31
+# t = int(input())
+# print((t-32)*5/9)
+
+
+# 33
+# def new_func():
+#     x = int(input("Kilogrami:"))
+#     a = int(input("Narxi:"))
+#     y = int(input())
+#     b = a/x
+#     return b*y
+
+# print(new_func())
+
+
+# 34
+# def new_func():
+#     x = int(input("Shokolad kg:"))
+#     a = int(input("Shokolad narxi:"))
+#     y = int(input("Konfet kg:"))
+#     b = int(input("Konfet narxi:"))
+#     shokolad = a/x
+#     konfet = b*y
+#     return shokolad-konfet
+
+# print(new_func())
+
+
+# 35
+# def new_func():
+#     v = int(input())
+#     u = int(input())
+#     t1 = int(input())
+#     t2 = int(input())
+#     return ((v+u)*t1)+((v-u)*t2)
+
+# print(new_func())
+
+
+# 36
+# v1 = int(input())
+# v2 = int(input())
+# s = int(input())
+# t = int(input())
+# a = s-t*v1
+# b = s*t*v2
+# print(a-b)
+
+
+# 38
+# def new_func():
+#     a = int(input())
+#     b = int(input())
+#     x = -b/a
+#     return x
+
+# print(new_func())
+
+
+# 39
+# import math
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = b**2-4*a*c 
+# x1 = -(b+math.sqrt(d))/2*a
+# x2 = -(b-math.sqrt(d))/2*a
+# print(f"x1={x1} x2={x2}")
