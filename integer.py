827bb26acf2a13ff6b43881267f07a48d531d304
Fayozbek <120027251+fayzullayevv@users.noreply.github.com>
2023-03-03 14:45:14 +0500

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,237 @@
+# 1
+# def metr(l):
+#     return f"{l//100}metr {l%100}sm"
+
+# print(metr(l=int(input())))
+
+
+# 2
+# def tonna(m):
+#     return f"{m//1000}tona {m%1000}kg"
+
+# print(tonna(m=int(input())))
+
+
+# 3
+# def bayt(b):
+#     return f"{b//1024}Kb {b%1024}bayt"
+
+# print(bayt(b=int(input())))
+
+
+# 4
+# def new_func():
+#     a = int(input())
+#     b = int(input())
+#     return (f"{b//a} marta")
+
+# print(new_func())
+
+
+# 5
+# def new_func():
+#     a = int(input())
+#     b = int(input())
+#     return (f"{b%a} joylashmagan")
+
+# print(new_func())
+
+
+# 6
+# def new_func():
+#     a = int(input("Ikki xonali son kiriting:"))
+#     son1 = a//10
+#     son2 = a%10
+#     return (f"O'nlik: {son2}, Birlik: {son1}")
+
+# print(new_func())
+
+
+# 7
+# def new_func():
+#     a = int(input("Ikki xonali son kiriting:"))
+#     son1 = a//10
+#     son2 = a%10
+#     return son1+son2
+
+# print(new_func())
+
+
+# 8
+# def new_func():
+#     a = int(input("Ikki xonali son kiriting:"))
+#     son1 = a//10
+#     son2 = a%10
+#     return f"{son2}{son1}"
+
+# print(new_func())
+
+
+# 9
+# def new_func():
+#     a = int(input("Uch xonali son kiriting:"))
+#     son = a%10
+#     return son
+
+# print(new_func())
+
+
+# 10
+# def new_func():
+#     a = int(input("Uch xonali son kiriting:"))
+#     son1 = a//100
+#     son2 = a//10%10
+#     return son1,son2
+
+# print(new_func())
+
+
+# 11
+# def new_func():
+#     a = int(input("Uch xonali son kiriting:"))
+#     son1 = a//100
+#     son2 = a//10%10
+#     son3 = a%10
+#     return son1+son2+son3
+
+# print(new_func())
+
+
+# 12
+# def new_func():
+#     a = int(input("Uch xonali son kiriting:"))
+#     son1 = a//100
+#     son2 = a//10%10
+#     son3 = a%10
+#     return f"{son3}{son2}{son1}"
+
+# print(new_func())
+
+
+# 13
+# def new_func():
+#     a = int(input("Uch xonali son kiriting:"))
+#     son1 = a//100
+#     son2 = a//10%10
+#     son3 = a%10
+#     return f"{son3}{son2}{son1}"
+
+# print(new_func())
+
+
+# 14
+# def new_func():
+#     a = int(input("Uch xonali son kiriting:"))
+#     son1 = a//100
+#     son2 = a//10%10
+#     son3 = a%10
+#     return f"{son3}{son1}{son2}"
+
+# print(new_func())
+
+
+# 15
+# def new_func():
+#     a = int(input("Uch xonali son kiriting:"))
+#     son1 = a//100
+#     son2 = a//10%10
+#     son3 = a%10
+#     return f"{son2}{son1}{son3}"
+
+# print(new_func())
+
+
+# 16
+# def new_func():
+#     a = int(input("Uch xonali son kiriting:"))
+#     son1 = a//100
+#     son2 = a//10%10
+#     son3 = a%10
+#     return f"{son1}{son3}{son2}"
+
+# print(new_func())
+
+
+# 17
+# def new_func():
+#     a = int(input("To'rt xonali son kiriting:"))
+#     son = a//10%10
+#     return son
+
+# print(new_func())
+
+
+# 18
+# def new_func():
+#     a = int(input("To'rt xonali son kiriting:"))
+#     son = a%10
+#     return son
+
+# print(new_func())
+
+
+# 19
+# def new_func():
+#     n = int(input())
+#     minut = n//60
+#     sekund = n%60
+#     return f"{minut}minut {sekund} sekund"
+
+# print(new_func())
+
+
+# 20
+# def new_func():
+#     n = int(input())
+#     soat = n//3600
+#     minut = n%60
+#     sekund = minut%60
+#     return f"{soat}soat {sekund} sekund"
+
+# print(new_func())
+
+
+# 21 va 22 Ushu masalalar qilingan 19 va 20da
+
+
+# 23
+# def new_func():
+#     n = int(input())
+#     soat = n//3600
+#     minut = n%60
+#     sekund = minut%60
+#     return f"{soat}soat {minut}minut {sekund}sekund"
+
+# print(new_func())
+
+
+# 24
+# def Hafta_kunlar(k):
+#     hafta = {
+#         0:'yakshanba',
+#         1:'dushanba',
+#         2:'seshanba',
+#         3:'chorshanba',
+#         4:'payshanba',
+#         5:'juma',
+#         6:'shanba',
+#     }
+#     if k%7==0:
+#         return (hafta[0])
+#     elif k%7==1:
+#         return (hafta[1])
+#     elif k%7==2:
+#         return (hafta[2])
+#     elif k%7==3:
+#         return (hafta[3])
+#     elif k%7==4:
+#         return (hafta[4])
+#     elif k%7==5:
+#         return (hafta[5])
+#     elif k%7==6:
+#         return (hafta[6])
+
+# print(Hafta_kunlar(k=int(input())))
+
+
+# 25
