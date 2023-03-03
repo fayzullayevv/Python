d09b1497e619d08307c0f4dda1146c209ad95e03
Fayozbek <120027251+fayzullayevv@users.noreply.github.com>
2023-03-03 15:19:03 +0500

    Add files via upload

---
+++ for.py
@@ -0,0 +1,143 @@
+#1
+# k = int(input())
+# n = int(input())
+# for i in range(n):
+#     print(k)
+
+#2
+# a = int(input())
+# b = int(input())
+# for i in range(a,b+1):
+#     print(i)
+
+
+#3
+# a = int(input())
+# b = int(input())
+# for i in range(b,a+1):
+#     print(i)
+
+
+#4
+# a = int(input())
+# for i in range(11):
+#     print(i*a)
+
+
+
+
+#7
+# s = 0 
+# a = int(input())
+# b = int(input())
+# for i in range(a,b+1):
+#     s+=i
+# print(s)
+
+
+#8
+# s = 1
+# a = int(input())
+# b = int(input())
+# for i in range(a,b+1):
+#     s*=i
+# print(s)
+
+
+#9
+# s = 1
+# a = int(input())
+# b = int(input())
+# for i in range(a,b+1):
+#     s=i**2
+# print(s)
+
+
+#10
+# a = 0
+# n = int(input())
+# for i in range(1,n+1):
+#     s=1/i
+#     a=a+s
+# print(a)
+
+
+#11
+# a = 0
+# n = int(input())
+# for i in range(1,n+1):
+#     s=(i+1)**2
+#     a=a+s
+# print(a)
+
+
+#15
+# a = int(input())
+# n = int(input())
+# print(a**n)
+
+
+#16
+# a = int(input())
+# n = int(input())
+# for i in range(1,n+1):
+#     print(a**i)
+
+
+#17
+# s = 0
+# a = int(input())
+# n = int(input())
+# for i in range(1,n+1):
+#     f=a**i
+#     s=s+f
+# print(s)
+
+
+#18
+# s = 0
+# a = int(input())
+# n = int(input())
+# for i in range(1,n+1):
+#     a = a**i
+#     s+=1
+# print(s)
+
+
+#19
+# a = 1
+# n = int(input())
+# for i in range(1,n+1):
+#     print(i*a)
+
+
+#20
+# a = 1
+# s = 0 
+# n = int(input())
+# for i in range(1,n+1):
+#     a = a*i
+#     s = s+a
+# print(s)
+
+
+#21
+# a = 1
+# s = 0 
+# n = int(input())
+# for i in range(1,n+1):
+#     a = 1/(a*i)
+#     s = s+a
+# print(s)
+
+
+#22
+# a = 1
+# s = 0 
+# x = int(input())
+# n = int(input())
+# for i in range(1,n+1):
+#     a = x**i/(a*i)
+#     s = s+a
+# print(s)
+
